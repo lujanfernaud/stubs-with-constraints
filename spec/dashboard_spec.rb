58a94b722e5d1f23f49cc323d4b9ef8a743ca686
lujanfernaud <lujanfernaud@gmail.com>
2017-12-09 08:15:54 +0000

    Rename visible_posts to user_posts

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -4,13 +4,13 @@ require "dashboard"
 describe Dashboard do
   describe "#posts" do
     it "returns posts visible to the current user" do
-      user          = create(:user)
-      visible_posts = double("visible_posts")
-      dashboard     = Dashboard.new(posts: Post.all, user: user)
+      user       = create(:user)
+      user_posts = double("user_posts")
+      dashboard  = Dashboard.new(posts: Post.all, user: user)
 
-      allow(Post).to receive(:visible_to).with(user).and_return(visible_posts)
+      allow(Post).to receive(:visible_to).with(user).and_return(user_posts)
 
-      expect(dashboard.posts).to eq(visible_posts)
+      expect(dashboard.posts).to eq(user_posts)
     end
   end
 end
